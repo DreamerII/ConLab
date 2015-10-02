58e0eae6eb14ef7819fc701509744aa48c5a9af7
dreamfire <yaruk@mail.ua>
2015-10-02 17:11:48 +0300

    new dialog animation

--- app/src/main/java/com/android/denysyuk/conlab/ui/activities/HomeActivity.java
+++ app/src/main/java/com/android/denysyuk/conlab/ui/activities/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.android.denysyuk.conlab.ui.activities;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
